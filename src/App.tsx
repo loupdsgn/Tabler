b02028e9f0ebdb697ed8cd75b55ae82271a314d7
loupdsgn <112827152+loupdsgn@users.noreply.github.com>
2024-12-19 08:05:57 -0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+    
+    function App() {
+      return (
+        <div>
+          <h1>Hello, Bootstrap!</h1>
+          <button className="btn btn-primary">Primary Button</button>
+        </div>
+      );
+    }
+    
+    export default App;
